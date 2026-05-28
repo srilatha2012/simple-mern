08aa0b0870e36de61b3c74f84910fc62c9249ca9
srilatha2012 <srilathp2012@gmail.com>
2026-05-28 17:21:51 -0400

    Remove dotenv from Render deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-require("dotenv").config();
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
