40281b79bad46b7dc978957402313eff8a8739a2
srilatha2012 <srilathp2012@gmail.com>
2026-05-28 17:07:38 -0400

    Add mongoDB connection log

--- src/db.js
+++ src/db.js
@@ -2,4 +2,14 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI);
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.log("MongoDB connection error:", err.message);
+  });
+
 module.exports = mongoose;
